1a8495965809f64c06fb24d3661635c9b86bf4dc
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2024-11-20 15:53:23 -0700

    Game complete at console level, reorganized script
    Finished the game using new function playGame(). This function
    runs the playRound(), function 5 times using a basic while loop.
    Had some difficulty thinking I would need to drastically change
    certain parts of the code in order to incorporate the variables
    humanSelection and computerSelection to avoid the issue of having
    their returns not be newly generated. Classic case of overthinking!

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 //To test linking JS externally
-
 console.log("Testing! 123!")
 
-// Declare the players score variables
-let humanScore = 0;
-let computerScore = 0;
+
 
 // Getting an RPS choice from the computer
 // write a function that randomly returns rock paper or scissors
 // 1. generate a random number between 0, 1, and 2.
 // 2. add if else statements for each condition.
-// 3. add a condition if this fails for some reason.
-
 function getComputerChoice() {
     let compChoiceNum = Math.floor(Math.random() * 3)
     if (compChoiceNum == 0) {
@@ -20,48 +15,83 @@ function getComputerChoice() {
         return "paper"
     } else if (compChoiceNum == 2) {
         return "scissors"
-    } else {
-        return "spock!"
     }
 }
 
 // Get RPS choice from Human player
 // valid choices are "Rock" , "Paper" , "Scissors"
 // add case insensitivity! 
-
 function getHumanChoice() {
     let playerString = prompt("Rock, Paper, or Scissors?")
     let lowerCaseString = playerString.toLowerCase();
     return lowerCaseString;
 }
 
-// Take the human and computer player choices as arguments
-// Plays a single round of rock paper scissors ()
-// Increments the round winner's score
-// logs a winner announcement
+// playGame combines functions from previous steps to run the entire game at once five times.
+// Uses a while loop to make sure the game is played 5 times.
 
-function playRound (humanChoice, computerChoice) {
-    if (humanSelection == computerSelection) {
-        console.log(`Draw! ${humanSelection} and ${computerSelection} are the same!`)
-    } else if (humanSelection == "rock" && computerSelection == "paper"){
-        console.log(`You lose! ${computerSelection} covers ${humanSelection}.`)
-        computerScore++
-    } else if (humanSelection == "rock" && computerSelection == "scissors"){
-        console.log(`You win! ${humanSelection} smashes ${computerSelection}!`)
-        humanScore++
-    } else if (humanSelection == "paper" && computerSelection == "rock"){
-        console.log(`You win! ${humanSelection} covers ${computerSelection}.`)
-        humanScore++
-    } else if (humanSelection == "paper" && computerSelection == "scissors"){
-        console.log(`You lose! ${computerSelection} cuts ${humanSelection}!`)
-        computerScore++
-    } else if (humanSelection == "scissors" && computerSelection == "rock"){
-        console.log(`You lose! ${computerSelection} smashes ${humanSelection}!`)
-        computerScore++
-    } else if (humanSelection == "scissors" && computerSelection == "paper"){
-        console.log(`You win! ${humanSelection} cuts ${computerSelection}!`)
-    } else {console.log("something has gone horribly wrong, try again!")}
+function playGame(){
+    // Declare the players score variables
+    let humanScore = 0;
+    let computerScore = 0;
+
+
+    // loop to run 5 rounds of the game 
+    let counter = 0;
+    while (counter < 5){
+        playRound();
+        counter++;
+    }
+    if (counter == 5) {
+        evalWinner();
+    }
+
+    // Take the human and computer player choices as arguments
+    // Plays a single round of rock paper scissors ()
+    // Increments the round winner's score
+    // logs a winner announcement
+    function playRound (humanChoice, computerChoice) {    
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        if (humanSelection == computerSelection) {
+            alert(`Draw! ${humanSelection} and ${computerSelection} are the same!`)
+            console.log("Draw");
+        } else if (humanSelection == "rock" && computerSelection == "paper"){
+            alert(`You lose! ${computerSelection} covers ${humanSelection}.`)
+            console.log("Lose");
+            computerScore++;
+        } else if (humanSelection == "rock" && computerSelection == "scissors"){
+            alert(`You win! ${humanSelection} smashes ${computerSelection}!`)
+            console.log("Win");
+            humanScore++;
+        } else if (humanSelection == "paper" && computerSelection == "rock"){
+            alert(`You win! ${humanSelection} covers ${computerSelection}.`)
+            console.log("Win");
+            humanScore++;
+        } else if (humanSelection == "paper" && computerSelection == "scissors"){
+            alert(`You lose! ${computerSelection} cuts ${humanSelection}!`)
+            console.log("Lose")
+            computerScore++;
+        } else if (humanSelection == "scissors" && computerSelection == "rock"){
+            alert(`You lose! ${computerSelection} smashes ${humanSelection}!`)
+            console.log("Lose");
+            computerScore++;
+        } else if (humanSelection == "scissors" && computerSelection == "paper"){
+            alert(`You win! ${humanSelection} cuts ${computerSelection}!`)
+            console.log("Win");
+            humanScore++;
+        } else {alert("something has gone horribly wrong, try again!")}
+    }
+
+    //Evaluates a winner based on the score values
+    function evalWinner(){
+        if (humanScore > computerScore){
+            alert("Congratulations! You acheived supreme victory over the machine race!")
+        } else if (humanScore < computerScore){
+            alert("Sadly, the machine race has triumphed over mankind. What fate have you doomed us to?")
+        } else if (humanScore == computerScore){
+            alert("A draw? But how are we ever going to settle this whole man vs. machine thing?!")
+        }
+    }
+    
 }
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
