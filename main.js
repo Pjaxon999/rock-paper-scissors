16dee8240cb7ab03e97168bc54a709ba050d9c9d
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2024-11-20 09:35:33 -0700

    Added function for computer opponent RPS choice
    Added fuction getComputerChoice. This function takes a number randomly
    generated from Math.random() and Math.Floor(). Then based on this value
    returns either "Rock", "Paper", or "Scissors". I am not 100% sure if this
    method is entirely legitimate, but it did produce good results in the
    console environment.

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("This is a test! If you can see this then Peter Jackson loves his girlfriend so much that he is willing to learn how to add JS to pages even though it makes him want to kill himself!")
+console.log("Testing! 123!")
+
+function getComputerChoice() {
+    let compChoiceNum = Math.floor(Math.random() * 3)
+    if (compChoiceNum == 0) {
+        return "Rock"
+    } else if (compChoiceNum == 1) {
+        return "Paper"
+    } else if (compChoiceNum == 2) {
+        return "Scissors"
+    } else {
+        return "Spock!"
+    }
+}
+
+// write a function that randomly returns rock paper or scissors
+// 1. generate a random number
+// 2. if random number is 1/3 of the range, return "rock"
+// 3 and 4. do the same for the other 2/3 but for "paper" and "scissors"
