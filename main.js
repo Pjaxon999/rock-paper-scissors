9350d6b43e6d3992f23f1b58bf4f8d5b8dc636f6
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2024-11-20 10:21:56 -0700

    Added function to play a round of RPS
    Added function playRound. This function takes the human and computer
    choices and compares them. It will then log a win, lose, or draw depending
    on the chosen string and the random string generated. It then increments the
    scores of the winner (but obviously not for a draw).

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 //To test linking JS externally
+
 console.log("Testing! 123!")
+
+// Declare the players score variables
+let humanScore = 0;
+let computerScore = 0;
 
 // Getting an RPS choice from the computer
 // write a function that randomly returns rock paper or scissors
 // 1. generate a random number between 0, 1, and 2.
 // 2. add if else statements for each condition.
 // 3. add a condition if this fails for some reason.
+
 function getComputerChoice() {
     let compChoiceNum = Math.floor(Math.random() * 3)
     if (compChoiceNum == 0) {
-        return "Rock"
+        return "rock"
     } else if (compChoiceNum == 1) {
-        return "Paper"
+        return "paper"
     } else if (compChoiceNum == 2) {
-        return "Scissors"
+        return "scissors"
     } else {
-        return "Spock!"
+        return "spock!"
     }
 }
 
@@ -29,3 +35,33 @@ function getHumanChoice() {
     return lowerCaseString;
 }
 
+// Take the human and computer player choices as arguments
+// Plays a single round of rock paper scissors ()
+// Increments the round winner's score
+// logs a winner announcement
+
+function playRound (humanChoice, computerChoice) {
+    if (humanSelection == computerSelection) {
+        console.log(`Draw! ${humanSelection} and ${computerSelection} are the same!`)
+    } else if (humanSelection == "rock" && computerSelection == "paper"){
+        console.log(`You lose! ${computerSelection} covers ${humanSelection}.`)
+        computerScore++
+    } else if (humanSelection == "rock" && computerSelection == "scissors"){
+        console.log(`You win! ${humanSelection} smashes ${computerSelection}!`)
+        humanScore++
+    } else if (humanSelection == "paper" && computerSelection == "rock"){
+        console.log(`You win! ${humanSelection} covers ${computerSelection}.`)
+        humanScore++
+    } else if (humanSelection == "paper" && computerSelection == "scissors"){
+        console.log(`You lose! ${computerSelection} cuts ${humanSelection}!`)
+        computerScore++
+    } else if (humanSelection == "scissors" && computerSelection == "rock"){
+        console.log(`You lose! ${computerSelection} smashes ${humanSelection}!`)
+        computerScore++
+    } else if (humanSelection == "scissors" && computerSelection == "paper"){
+        console.log(`You win! ${humanSelection} cuts ${computerSelection}!`)
+    } else {console.log("something has gone horribly wrong, try again!")}
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
