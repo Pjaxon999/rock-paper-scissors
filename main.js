f83331c623fb04d6f7c858728223e66b0a84f8bc
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2024-12-19 08:14:49 -0700

    Staging for new branch of project. Please Work!
    The goal of this project is to add a UI to the existing RPS game that
    was created earlier. I am essentially praying that this whole git branch
    stuff is going to work and that this will not mess things up but here
    we go! I added a CSS file to add some more cusomtization to the UI
    elements. Also removed some sections of code from the old JS file to
    facilitate the new system. Now comes the task of refactoring the JS to
    use DOM methods to manipulate the UI instead of playing the game in the
    console.

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 console.log("Testing! 123!")
 
 
-
 // Getting an RPS choice from the computer
 // write a function that randomly returns rock paper or scissors
 // 1. generate a random number between 0, 1, and 2.
@@ -18,6 +17,7 @@ function getComputerChoice() {
     }
 }
 
+
 // Get RPS choice from Human player
 // valid choices are "Rock" , "Paper" , "Scissors"
 // add case insensitivity! 
@@ -27,71 +27,61 @@ function getHumanChoice() {
     return lowerCaseString;
 }
 
-// playGame combines functions from previous steps to run the entire game at once five times.
-// Uses a while loop to make sure the game is played 5 times.
 
-function playGame(){
-    // Declare the players score variables
-    let humanScore = 0;
-    let computerScore = 0;
-
-
-    // loop to run 5 rounds of the game 
-    let counter = 0;
-    while (counter < 5){
-        playRound();
-        counter++;
-    }
-    if (counter == 5) {
-        evalWinner();
-    }
-
-    // Take the human and computer player choices as arguments
-    // Plays a single round of rock paper scissors ()
-    // Increments the round winner's score
-    // logs a winner announcement
-    function playRound (humanChoice, computerChoice) {    
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        if (humanSelection == computerSelection) {
-            alert(`Draw! ${humanSelection} and ${computerSelection} are the same!`)
-            console.log("Draw");
-        } else if (humanSelection == "rock" && computerSelection == "paper"){
-            alert(`You lose! ${computerSelection} covers ${humanSelection}.`)
-            console.log("Lose");
-            computerScore++;
-        } else if (humanSelection == "rock" && computerSelection == "scissors"){
-            alert(`You win! ${humanSelection} smashes ${computerSelection}!`)
-            console.log("Win");
-            humanScore++;
-        } else if (humanSelection == "paper" && computerSelection == "rock"){
-            alert(`You win! ${humanSelection} covers ${computerSelection}.`)
-            console.log("Win");
-            humanScore++;
-        } else if (humanSelection == "paper" && computerSelection == "scissors"){
-            alert(`You lose! ${computerSelection} cuts ${humanSelection}!`)
-            console.log("Lose")
-            computerScore++;
-        } else if (humanSelection == "scissors" && computerSelection == "rock"){
-            alert(`You lose! ${computerSelection} smashes ${humanSelection}!`)
-            console.log("Lose");
-            computerScore++;
-        } else if (humanSelection == "scissors" && computerSelection == "paper"){
-            alert(`You win! ${humanSelection} cuts ${computerSelection}!`)
-            console.log("Win");
-            humanScore++;
-        } else {alert("something has gone horribly wrong, try again!")}
-    }
-
-    //Evaluates a winner based on the score values
-    function evalWinner(){
-        if (humanScore > computerScore){
-            alert("Congratulations! You acheived supreme victory over the machine race!")
-        } else if (humanScore < computerScore){
-            alert("Sadly, the machine race has triumphed over mankind. What fate have you doomed us to?")
-        } else if (humanScore == computerScore){
-            alert("A draw? But how are we ever going to settle this whole man vs. machine thing?!")
-        }
-    }
-    
+// Take the human and computer player choices as arguments
+// Plays a single round of rock paper scissors ()
+// Increments the round winner's score
+// logs a winner announcement
+function playRound (humanChoice, computerChoice) {    
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    if (humanSelection == computerSelection) {
+        alert(`Draw! ${humanSelection} and ${computerSelection} are the same!`)
+        console.log("Draw");
+    } else if (humanSelection == "rock" && computerSelection == "paper"){
+        alert(`You lose! ${computerSelection} covers ${humanSelection}.`)
+        console.log("Lose");
+        computerScore++;
+    } else if (humanSelection == "rock" && computerSelection == "scissors"){
+        alert(`You win! ${humanSelection} smashes ${computerSelection}!`)
+        console.log("Win");
+        humanScore++;
+    } else if (humanSelection == "paper" && computerSelection == "rock"){
+        alert(`You win! ${humanSelection} covers ${computerSelection}.`)
+        console.log("Win");
+        humanScore++;
+    } else if (humanSelection == "paper" && computerSelection == "scissors"){
+        alert(`You lose! ${computerSelection} cuts ${humanSelection}!`)
+        console.log("Lose")
+        computerScore++;
+    } else if (humanSelection == "scissors" && computerSelection == "rock"){
+        alert(`You lose! ${computerSelection} smashes ${humanSelection}!`)
+        console.log("Lose");
+        computerScore++;
+    } else if (humanSelection == "scissors" && computerSelection == "paper"){
+        alert(`You win! ${humanSelection} cuts ${computerSelection}!`)
+        console.log("Win");
+        humanScore++;
+    } else {alert("something has gone horribly wrong, try again!")}
 }
+
+
+//Evaluate a winner based on the score after 5 rounds
+function evalWinner(){
+    if (humanScore > computerScore){
+        alert("Congratulations! You acheived supreme victory over the machine race!")
+    } else if (humanScore < computerScore){
+        alert("Sadly, the machine race has triumphed over mankind. What fate have you doomed us to?")
+    } else if (humanScore == computerScore){
+        alert("A draw? But how are we ever going to settle this whole man vs. machine thing?!")
+    }
+}
+    
+//Event listeners for buttons
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+
+rockBtn.addEventListener("click", playRound);
+paperBtn.addEventListener("click", playRound);
+scissorsBtn.addEventListener("click", playRound);
